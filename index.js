b3ec0188f5fac114f601940b440927dd565a0bcb
BishwaJit07 <bishwajitr50@gmail.com>
2023-08-13 14:38:24 +0600

    server fixed

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 // mongo db 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://SoulMate-Matrimony:ksfIn7UGTTU4vOYq@cluster0.gq6gqcm.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.pmqtpdf.mongodb.net/?retryWrites=true&w=majority`;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -25,50 +26,47 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const usersCollection = client.db("SoulMate-Matrimony").collection("users");
-const coupleCollection = client.db("SoulMate-Matrimony").collection("CoupleData");
- 
-
-
-//user get point
- app.get("/users", async (req, res) => {
-        const result = await usersCollection.find().toArray();
-        return res.send(result);
-      });
-
-pp.post("/users",  async (req, res) => {
-        const user = req.body;
-        const query = { email: user.email };
-  
-        const excitingUser = await usersCollection.findOne(query);
-        console.log("existing User", excitingUser);
-  
-        if (excitingUser) {
-          return res.send({ message: "user exists" });
-        }
-        const result = await usersCollection.insertOne(user);
-        return res.send(result);
-      });
-
-
- app.get("/allCouple", async (req, res) => {
-        const result = await coupleCollection.find().toArray();
-        return res.send(result);
-      });
-
-
+    const coupleCollection = client.db("SoulMate-Matrimony").collection("CoupleData");
+     
+    
+    
+    //user get point
+     app.get("/users", async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            return res.send(result);
+          });
+    
+    app.post("/users",  async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+      
+            const excitingUser = await usersCollection.findOne(query);
+            console.log("existing User", excitingUser);
+      
+            if (excitingUser) {
+              return res.send({ message: "user exists" });
+            }
+            const result = await usersCollection.insertOne(user);
+            return res.send(result);
+          });
+    
+    
+     app.get("/allCouple", async (req, res) => {
+            const result = await coupleCollection.find().toArray();
+            return res.send(result);
+          });
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   }
-  
- catch (error) {
-  console.error("MongoDB connection error:", error);
-}
-  
-  finally {
+  catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+   finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
