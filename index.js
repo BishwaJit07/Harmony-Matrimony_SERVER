f14d3c6f65c5bba9f2f6ecc352e55e4da2c5bc6e
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-08-21 12:33:10 +0600

    get hotel and catering data

--- index.js
+++ index.js
@@ -144,16 +144,26 @@ async function run() {
       res.send(result)
     })
 
-    // get services data
+    // get photography services data
     app.get('/service/photography', async(req, res) => {
       const query = { category: "photography" };
       const result = await serviceCollection.find(query).toArray()
       res.send(result)
     })
 
+    app.get("/service/hotel", async (req, res) => {
+      const query = { category: "hotel" };
+      const result = await serviceCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/service/catering", async (req, res) => {
+      const query = { category: "catering" };
+      const result = await serviceCollection.find(query).toArray();
+      res.send(result);
+    });
+
     
-         
-        
 
           //blogs related api
 
