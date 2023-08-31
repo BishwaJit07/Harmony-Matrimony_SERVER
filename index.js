5fe8a08c260ae6714fda15e15bdb5e33ad66ab85
Santo1997 <hossainsanto2@gmail.com>
2023-08-31 18:09:24 +0600

    latestblog by samiya apu

--- index.js
+++ index.js
@@ -65,10 +65,10 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const usersCollection = client
-    .db("SoulMate-Matrimony").collection("users");
+    const usersCollection = client.db("SoulMate-Matrimony").collection("users");
     const authorityCollection = client
-    .db("SoulMate-Matrimony").collection("authority");
+      .db("SoulMate-Matrimony")
+      .collection("authority");
     const coupleCollection = client
       .db("SoulMate-Matrimony")
       .collection("CoupleData");
@@ -84,12 +84,13 @@ async function run() {
       .db("SoulMate-Matrimony")
       .collection("paymentHistory");
 
-    const orderCollection = client
-    .db("SoulMate-Matrimony").collection("order");
+    const orderCollection = client.db("SoulMate-Matrimony").collection("order");
     const reviewCollection = client
-    .db("SoulMate-Matrimony").collection("review");
+      .db("SoulMate-Matrimony")
+      .collection("review");
     const teamMemberCollection = client
-    .db("SoulMate-Matrimony").collection("meetourteam");
+      .db("SoulMate-Matrimony")
+      .collection("meetourteam");
 
     // JWt
 
@@ -102,7 +103,6 @@ async function run() {
     const statusCollection = client
       .db("SoulMate-Matrimony")
       .collection("statusPost");
-
 
     // JWt
     app.post("/jwt", (req, res) => {
@@ -329,8 +329,8 @@ async function run() {
       const updateDoc = {
         $set: {
           profile_complete: updateInfo.profile_complete,
-          aboutMe : updateInfo.aboutMe,
-          interests : updateInfo.hobbies
+          aboutMe: updateInfo.aboutMe,
+          interests: updateInfo.hobbies,
         },
       };
       const options = { upsert: true };
@@ -362,7 +362,6 @@ async function run() {
       const result = await usersCollection.find().toArray();
       return res.send(result);
     });
-  
 
     app.get("/specificUser/:id", async (req, res) => {
       const id = req.params.id;
@@ -384,12 +383,11 @@ async function run() {
       return res.send(result);
     });
 
-    //  team Members 
+    //  team Members
     app.get("/team", async (req, res) => {
       const result = await teamMemberCollection.find().toArray();
       return res.send(result);
     });
-
 
     app.post("/authority", async (req, res) => {
       const user = req.body;
@@ -424,25 +422,24 @@ async function run() {
       res.send(result);
     });
 
-
     app.post("/contact", async (req, res) => {
       const contactData = req.body;
       const result = await contactCollection.insertOne(contactData);
       res.send(result);
     });
 
-        //  user review
-        app.get("/reviews", async (req, res) => {
-          const result = await reviewCollection.find().toArray();
-          return res.send(result);
-        });
-    
-        app.post("/reviews", async (req, res) => {
-          const newreview = req.body;
-          console.log(newreview);
-          const result = await reviewCollection.insertOne(newstory);
-          return res.send(result);
-        });
+    //  user review
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      return res.send(result);
+    });
+
+    app.post("/reviews", async (req, res) => {
+      const newreview = req.body;
+      console.log(newreview);
+      const result = await reviewCollection.insertOne(newstory);
+      return res.send(result);
+    });
 
     // get photography services data
     app.get("/service/photography", async (req, res) => {
@@ -545,6 +542,13 @@ async function run() {
         sort: { react: -1 },
       };
       const result = await blogsCollection.find(query, options).toArray();
+      return res.send(result);
+    });
+
+    app.get("/blogsLatest", async (req, res) => {
+      const query = {};
+      sortBy = { _id: -1 };
+      const result = await blogsCollection.find(query).sort(sortBy).toArray();
       return res.send(result);
     });
 
